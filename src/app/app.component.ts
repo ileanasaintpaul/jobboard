1d758ae31779c25f9eb3096be19a7c3c7921a3ad
ileanasaintpaul <ileana.saint-paul@epitech.eu>
2024-10-09 17:00:01 +0200

    [ADD] - finish log in page and sign up page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { HomepageComponent } from "./homepage/homepage.component";
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
 	selector: "app-root",
 	standalone: true,
-	imports: [HomepageComponent],
+	imports: [RouterOutlet, NavbarComponent],
 	templateUrl: "./app.component.html",
 	styleUrl: "./app.component.scss",
 })
