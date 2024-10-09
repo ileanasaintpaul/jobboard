7a4cce3b141ed410dd69cff607cca4d1a168ba15
ileanasaintpaul <ileana.saint-paul@epitech.eu>
2024-10-09 10:23:24 +0200

    [ADD] - finish design homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { HomepageComponent } from "./homepage/homepage.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: "app-root",
+	standalone: true,
+	imports: [HomepageComponent],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'jobboard';
+	title = "jobboard";
 }
